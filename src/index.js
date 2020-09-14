e9ed67a1b60352d7bae04ef936c08f0867a532a8
Aliznye <69394138+Aliznye@users.noreply.github.com>
2020-09-13 22:07:59 -0400

    completed date/time stamps

--- src/index.js
+++ src/index.js
@@ -1,24 +1,49 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${minute}`;
+      }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+      }
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
-    console.log(response);
     let h1 = document.querySelector("#current-city");
-    h1.innerHTML = (response.data.name);
     let currentTemp = Math.round(response.data.main.temp)
     let temperature = document.querySelector("#temperature");
-    temperature.innerHTML = (currentTemp);
     let description = document.querySelector("#description");
-    description.innerHTML = (response.data.weather[0].main);
     let precepitation = document.querySelector("#precipitation");
     let humidity = document.querySelector("#humidity");
-    humidity.innerHTML = (response.data.main.humidity);
     let wind = document.querySelector("#wind-speed");
-    wind.innerHTML = (response.data.wind.speed);
     let feelsTemp = Math.round(response.data.main.feels_like);
     let feelsLike = document.querySelector("#feels-like");
+    let date = document.querySelector("#date")
+    h1.innerHTML = (response.data.name);
+    temperature.innerHTML = (currentTemp);
+    description.innerHTML = (response.data.weather[0].main);
+    humidity.innerHTML = (response.data.main.humidity);
+    wind.innerHTML = (response.data.wind.speed);
     feelsLike.innerHTML = (feelsTemp);
+    date.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "617eafff664cc7b609a6d20494a9e0cf";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=imperial`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
+
 
 
 axios.get(apiUrl).then(displayTemperature);
